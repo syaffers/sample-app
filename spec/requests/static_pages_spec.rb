6ace4b8fde4b6cf99174db5afab37d9e7fd2bc11
syaffers <syafiqkamarulazman@hotmail.com>
2013-10-05 17:10:02 +0800

    Finished static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    
+    it "should have the right title" do
+      visit "/static_pages/home"
+      expect(page).to have_title('Home')
+    end
   end
   
   describe "Help page" do
@@ -15,12 +20,22 @@ describe "static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    
+    it "should have the right title" do
+      visit "/static_pages/help"
+      expect(page).to have_title('Help')
+    end
   end
   
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    
+    it "should have the right title" do
+      visit "/static_pages/about"
+      expect(page).to have_title('About Us')
     end
   end
 end
