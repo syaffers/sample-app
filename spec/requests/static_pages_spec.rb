059def42a668f39e101b57d7c7b106293058936f
syaffers <syafiqkamarulazman@hotmail.com>
2013-10-05 17:24:45 +0800

    Added contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,5 +38,17 @@ describe "static pages" do
       expect(page).to have_title('About Us')
     end
   end
+  
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact Us')
+    end
+  end
 end
 
