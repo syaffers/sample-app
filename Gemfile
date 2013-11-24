a118549d799408ec25c0f1a988abff37dfe03b6e
syaffers <syafiqkamarulazman@hotmail.com>
2013-11-25 00:16:09 +0800

    Finished user signup

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :test do
   gem 'selenium-webdriver', '2.37.0'
   gem 'capybara', '2.1.0'
   gem 'libnotify', '0.8.0'
+  gem 'factory_girl_rails', '4.2.1'
 end
 
 # Use SCSS for stylesheets
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Syafiq Kamarul Azman"
+    email     "syafiqkamarulazman@hotmail.com"
+    password  "admin123"
+    password_confirmation "admin123"
+  end
+end
