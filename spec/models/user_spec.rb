292ec09906f7592334a5ed5e2a2fc8ead3bf00ab
syaffers <syafiqkamarulazman@hotmail.com>
2013-11-24 21:56:04 +0800

    Added extra tests and changed user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -56,7 +56,12 @@ describe User do
   describe "when email format is invalid" do
     
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com 
+                     user_at_foo.org 
+                     example.user@foo. 
+                     foo@bar_baz.com 
+                     foo@bar+baz.com
+                     foo@bar..com ]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -83,6 +88,16 @@ describe User do
     end
     
     it { should_not be_valid }
+  end
+  
+  describe "email address with mixed cases" do
+    let(:mixed_case_email) { "FOo@ExAmPLe.coM" }
+    
+    it "should be saved as lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
   
   describe "with a password that's too short" do
