00ca676797cc2f9bdd48fda108c3fe96d35a1e4d
syaffers <syafiqkamarulazman@hotmail.com>
2014-02-18 21:55:18 +0800

    Changed project name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -19,4 +19,4 @@ def secure_token
   end
 end
 
-SampleApp::Application.config.secret_key_base = secure_token
+Transpub::Application.config.secret_key_base = secure_token
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SampleApp::Application.config.session_store :cookie_store, key: '_sample-app_session'
+Transpub::Application.config.session_store :cookie_store, key: '_transpub_session'
